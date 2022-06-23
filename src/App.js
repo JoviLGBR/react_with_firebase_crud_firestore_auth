084f511efb6c9dea30668e910e90ac99e0577029
João Galvão <joaogalvao3052@gmail.com>
2022-06-22 21:31:43 -0300

    CRUD completo com firestore e conectado ao firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "./firebaseConnection";
+import { useState, useEffect } from "react";
+import "./style.css";
 
 function App() {
+  const [idPost, setIdPost] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      await firebase
+        .firestore()
+        .collection("posts")
+        .onSnapshot((doc) => {
+          let meusPosts = [];
+
+          doc.forEach((item) => {
+            meusPosts.push({
+              id: item.id,
+              titulo: item.data().titulo,
+              autor: item.data().autor,
+            });
+          });
+
+          setPosts(meusPosts);
+        });
+    }
+
+    loadPosts();
+  }, []);
+
+  async function handleAdd() {
+    await firebase
+      .firestore()
+      .collection("posts")
+      .add({
+        titulo: titulo,
+        autor: autor,
+      })
+      .then(() => {
+        console.log("dados cadastrados com sucesso");
+        setAutor("");
+        setTitulo("");
+      })
+      .catch((error) => {
+        console.log("gerou algum erro!" + error);
+      });
+  }
+
+  async function buscaPost() {
+    await firebase
+      .firestore()
+      .collection("posts")
+      .doc(idPost)
+      .get()
+      .then((snapshot) => {
+        let lista = [];
+
+        setIdPost(snapshot.id);
+        setTitulo(snapshot.data().titulo);
+        setAutor(snapshot.data().autor);
+      })
+      .catch((error) => {
+        console.log("gerou algum erro!" + error);
+      });
+  }
+
+  async function editarPost() {
+    await firebase
+      .firestore()
+      .collection("posts")
+      .doc(idPost)
+      .update({
+        titulo: titulo,
+        autor: autor,
+      })
+      .then(() => {
+        console.log("dados alterados com sucesso");
+        setIdPost("");
+        setTitulo("");
+        setAutor("");
+      })
+      .catch((error) => {
+        console.log("gerou algum erro!" + error);
+      });
+  }
+
+  async function excluirPost(id){
+
+    firebase.firestore().collection('posts').doc(id).delete().then(
+      ()=>{
+        alert('O POST FOI EXCLUIDO!');
+      }
+    );
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReactJS + Firebase</h1>
+      <br />
+
+      <div className="container">
+        <label>ID: </label>
+        <input
+          type="text"
+          value={idPost}
+          onChange={(e) => setIdPost(e.target.value)}
+        ></input>
+
+        <label>Titulo: </label>
+        <textarea
+          type="text"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        ></textarea>
+
+        <label>Autor: </label>
+        <input
+          type="text"
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
+        ></input>
+
+        <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscaPost}>Buscar</button>
+        <button onClick={editarPost}>Editar Post</button>
+        <br />
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <span>ID: {post.id}</span>
+                <br />
+                <span>Titulo: {post.titulo}</span>
+                <br />
+                <span>Autor: {post.autor}</span>
+                <br />
+                <button onClick={() => excluirPost(post.id)}>Excluir post</button>
+                <br />
+                <br />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+let firebaseConfig = {
+  apiKey: "AIzaSyBzgBJASrYftcUgtM5VWLg-th0KnefZucY",
+  authDomain: "curso-react-3052.firebaseapp.com",
+  projectId: "curso-react-3052",
+  storageBucket: "curso-react-3052.appspot.com",
+  messagingSenderId: "333890286235",
+  appId: "1:333890286235:web:4b67220a0e0ed022398c3e",
+  measurementId: "G-X0Y5JQ6VKJ"
+};
+
+// Initialize Firebase
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
