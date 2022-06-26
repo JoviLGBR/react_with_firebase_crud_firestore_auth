add96322deab0ee0e390ee887f3c9ed0007a0903
João Galvão <joaogalvao3052@gmail.com>
2022-06-26 15:30:58 -0300

    adicionando autenticação

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [autor, setAutor] = useState("");
   const [posts, setPosts] = useState([]);
 
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [user, setUser] = useState(false);
+  const [userLogged, setUserLogged] = useState({});
+
   useEffect(() => {
     async function loadPosts() {
       await firebase
@@ -29,6 +34,25 @@ function App() {
     }
 
     loadPosts();
+  }, []);
+
+  useEffect(() => {
+    async function checkLogin() {
+      await firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          setUser(true);
+          setUserLogged({
+            uid: user.uid,
+            email: user.email,
+          });
+        } else {
+          setUser(false);
+          setUserLogged({});
+        }
+      });
+    }
+
+    checkLogin();
   }, []);
 
   async function handleAdd() {
@@ -87,14 +111,50 @@ function App() {
       });
   }
 
-  async function excluirPost(id){
+  async function excluirPost(id) {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(id)
+      .delete()
+      .then(() => {
+        alert("O POST FOI EXCLUIDO!");
+      });
+  }
 
-    firebase.firestore().collection('posts').doc(id).delete().then(
-      ()=>{
-        alert('O POST FOI EXCLUIDO!');
-      }
-    );
+  async function novoUsuario() {
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, senha)
+      .then((value) => {
+        alert("Cadastrado com sucesso!");
+      })
+      .catch((error) => {
+        if (error.code === "auth/weak-password") {
+          alert("Senha muito fraca...");
+        } else if (error.code === "auth/email-already-in-use") {
+          alert("Email já existente!");
+        } else if (error.code === "auth/invalid-email") {
+          alert("Endereço de email não é valido!");
+        }
+        //console.log('error ' + error);
+      });
+  }
 
+  async function logout() {
+    await firebase.auth().signOut();
+  }
+
+  async function fazerLogin() {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, senha)
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((error) => {
+        console.log("Erro ao fazer o Login " + error);
+      });
   }
 
   return (
@@ -102,7 +162,46 @@ function App() {
       <h1>ReactJS + Firebase</h1>
       <br />
 
+      {user && (
+        <div>
+          <strong>Seja bem vindo! Você está logado!</strong>
+          <span>
+            {userLogged.uid} - {userLogged.email}
+          </span>
+          <br />
+          <br />
+        </div>
+      )}
+
       <div className="container">
+        <label>Email: </label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <br />
+
+        <label>Senha: </label>
+        <input
+          type="password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        ></input>
+        <br />
+
+        <button onClick={fazerLogin}>Fazer Login</button>
+        <button onClick={novoUsuario}>Cadastrar</button>
+        <button onClick={logout}>Sair da Conta</button>
+
+        <br />
+      </div>
+
+      <hr />
+      <br />
+
+      <div className="container">
+        <h2>Banco de dados</h2>
         <label>ID: </label>
         <input
           type="text"
@@ -139,7 +238,9 @@ function App() {
                 <br />
                 <span>Autor: {post.autor}</span>
                 <br />
-                <button onClick={() => excluirPost(post.id)}>Excluir post</button>
+                <button onClick={() => excluirPost(post.id)}>
+                  Excluir post
+                </button>
                 <br />
                 <br />
               </li>
